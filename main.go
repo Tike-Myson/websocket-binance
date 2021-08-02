7c99b5c08c6d0fc8dc99294719c83b93d8c87a5a
Tike-Myson <asanxan.nurtilek@bk.ru>
2021-08-02 19:39:42 +0600

    needed sorting

--- main.go
+++ main.go
@@ -2,121 +2,83 @@ package main
 
 import (
 	"flag"
-	"html/template"
-	"log"
-	"net/http"
-
 	"github.com/gorilla/websocket"
+	"log"
+	"os"
+	"os/signal"
 )
 
-var addr = flag.String("addr", "localhost:8080", "http service address")
-
-var upgrader = websocket.Upgrader{} // use default options
-
-func echo(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}
-	defer c.Close()
-	for {
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
-		}
-		log.Printf("recv: %s", message)
-		err = c.WriteMessage(mt, []byte("Fuck off"))
-		if err != nil {
-			log.Println("write:", err)
-			break
-		}
-	}
+type Orders struct {
+	Stream   string `json:"stream"`
+	Data   struct {
+		LastUpdateID int64      `json:"lastUpdateId"`
+		Bids         [][]string `json:"bids"`
+		Asks         [][]string `json:"asks"`
+	} `json:"data"`
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
-}
+var (
+	Reset = "\033[0m"
+	Red   = "\033[31m"
+	Green = "\033[32m"
+)
+
+var addr = flag.String("addr", "localhost:8000", "http service address")
 
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
-	http.HandleFunc("/echo", echo)
-	http.HandleFunc("/", home)
-	log.Fatal(http.ListenAndServe(*addr, nil))
-}
 
-var homeTemplate = template.Must(template.New("").Parse(`
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<script>  
-window.addEventListener("load", function(evt) {
-    var output = document.getElementById("output");
-    var input = document.getElementById("input");
-    var ws;
-    var print = function(message) {
-        var d = document.createElement("div");
-        d.textContent = message;
-        output.appendChild(d);
-        output.scroll(0, output.scrollHeight);
-    };
-    document.getElementById("open").onclick = function(evt) {
-        if (ws) {
-            return false;
-        }
-        ws = new WebSocket("{{.}}");
-        ws.onopen = function(evt) {
-            print("OPEN");
-        }
-        ws.onclose = function(evt) {
-            print("CLOSE");
-            ws = null;
-        }
-        ws.onmessage = function(evt) {
-            print("RESPONSE: " + evt.data);
-        }
-        ws.onerror = function(evt) {
-            print("ERROR: " + evt.data);
-        }
-        return false;
-    };
-    document.getElementById("send").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        print("SEND: " + input.value);
-        ws.send(input.value);
-        return false;
-    };
-    document.getElementById("close").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        ws.close();
-        return false;
-    };
-});
-</script>
-</head>
-<body>
-<table>
-<tr><td valign="top" width="50%">
-<p>Click "Open" to create a connection to the server, 
-"Send" to send a message to the server and "Close" to close the connection. 
-You can change the message and send multiple times.
-<p>
-<form>
-<button id="open">Open</button>
-<button id="close">Close</button>
-<p><input id="input" type="text" value="Hello world!">
-<button id="send">Send</button>
-</form>
-</td><td valign="top" width="50%">
-<div id="output" style="max-height: 70vh;overflow-y: scroll;"></div>
-</td></tr></table>
-</body>
-</html>
-`))
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	url := "wss://stream.binance.com:9443/stream?streams=ethbtc@depth5"
+	log.Printf("connecting to %s", url)
+
+	c, _, err := websocket.DefaultDialer.Dial("wss://stream.binance.com:9443/stream?streams=btcusdt@depth20", nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			//var orders Orders
+			//json.Unmarshal(message, &orders)
+			//fmt.Printf("|%-9.2f|%-9.2f|\n", Green + "Price" + Reset, Green + "Amount" + Reset)
+			//for _, v := range orders.Data.Asks {
+			//	fmt.Printf("|%-9.2f|%-9.2f|\n", v[0], v[1])
+			//}
+			//fmt.Printf("|%-9.2f|%-9.2f|\n", Red + "Price" + Reset, Green + "Amount" + Reset)
+			//for _, v := range orders.Data.Bids {
+			//	fmt.Printf("|%-9.2f|%-9.2f|\n", v[0], v[1])
+			//}
+			//fmt.Println("*********************")
+			//fmt.Println("----------------------")
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	for {
+		select {
+		case <-done:
+			return
+		case <-interrupt:
+			log.Println("interrupt")
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			return
+		}
+	}
+}
